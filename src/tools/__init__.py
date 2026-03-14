8df569eeb96d20667c1c84485b7cabd8d4e28093
Yossapol Witayanont <reef_3llen@hotmail.com>
2026-03-15 05:33:02 +0700

    feat: :rocket: elasticsearch

--- src/tools/__init__.py
+++ src/tools/__init__.py
@@ -11,6 +11,7 @@ postgres_client = None
 redis_client = None
 kafka_client = None
 obsidian_client = None
+elasticsearch_client = None
 
 if config.enable_jira or config.enable_confluence or config.enable_bitbucket:
     from src.clients.atlassian import AtlassianClient
@@ -31,3 +32,12 @@ if config.enable_kafka:
 if config.enable_obsidian:
     from src.clients.obsidian import ObsidianClient
     obsidian_client = ObsidianClient(api_key=config.obsidian_api_key, base_url=config.obsidian_url)
+
+if config.enable_elasticsearch:
+    from src.clients.elasticsearch import ElasticsearchClient
+    elasticsearch_client = ElasticsearchClient(
+        url=config.elasticsearch_url,
+        api_key=config.elasticsearch_api_key,
+        username=config.elasticsearch_username,
+        password=config.elasticsearch_password
+    )
